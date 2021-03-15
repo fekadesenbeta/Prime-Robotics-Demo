44a923bc7255a4fc08249fcb89e7b50f1c6c0f3e
Fekade Senbeta <fekadesenbeta@gmail.com>
2021-03-15 09:17:33 -0600

    prime robotics demo site

---
+++ app.js
@@ -0,0 +1,16 @@
+const menu = document.querySelector('#mobile-menu');
+
+const menuLinks = document.querySelector('.navbar__menu');
+
+const navLogo = document.querySelector('#navbar__logo');
+
+
+// Mobile menu
+const mobileMenu = () => {
+    menu.classList.toggle('is-active');
+    menuLinks.classList.toggle('active');
+};
+
+menu.addEventListener('click', mobileMenu);
+
+
